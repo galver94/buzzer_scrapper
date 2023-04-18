aa61a5874d8ff3143440a7633a7a8e48ff1087f1
galver94 <jesusherrerobesteiro@gmail.com>
2023-04-18 23:52:09 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,4 @@
+NEWS_API_KEY = "0c7fac7c260c4af78c2b2cb01e9ff5f6"
+NEWS_API_ENDPOINT = "https://newsapi.org/v2/everything?"
+email_1 = "jesusherrerobesteiro@gmail.com"
+email_password = "news4Geeks"
